dc153b782ee17078cc115609f3368cd608bbac04
rublesKelly <rkellymkk@gmail.com>
2022-05-24 10:41:45 +0100

    fixed SQL error

--- Controller.cs
+++ Controller.cs
@@ -30,13 +30,13 @@ namespace Controller
         private void SetUpCommandParameters(SqlCommand command, AccountHolder accountHolder)
         {
             //Initialise a new sqlParameter and use the parameterless constructor
-            command.Parameters.Add(new SqlParameter()
-            {
-                ParameterName = "@ID",
-                Value = accountHolder.ID,
-                SqlDbType = SqlDbType.VarChar,
-                Size = 50
-            });
+            //command.Parameters.Add(new SqlParameter()
+            //{
+            //    ParameterName = "@ID",
+            //    Value = accountHolder.ID,
+            //    SqlDbType = SqlDbType.VarChar,
+            //    Size = 50
+            //});
 
             command.Parameters.Add(new SqlParameter()
             {
@@ -161,19 +161,23 @@ namespace Controller
         //Insert AccountHolder 
         public void InsertAccountHolder(AccountHolder accountHolder, out bool success)
         {
-            string sql = "INSERT INTO account_holder_table" +
+            string sql = "INSERT INTO account_holder_table " +
                 "(first_name, last_name, address, phone_num, email, password, date_of_birth) " +
-                "Values (@first_name, @last_name, @address, @phone_num, @email, @password, @date_of_birth)";
+                "Values(@first_name, @last_name, @address, @phone_num, @email, @password, @date_of_birth)";
             try
             {
                 //Set up command 
                 SqlCommand command = new SqlCommand(sql, connection);
+                SetUpCommandParameters(command, accountHolder);
 
                 //Open connection to DB
                 connection.Open();
+                
+                System.Diagnostics.Debug.WriteLine("Hello" + command.CommandText.ToString());
 
                 //Execute command and verify success with numRows
                 int numRows = command.ExecuteNonQuery();
+                
                 if (numRows > 0) success = true; else success = false;
             }
             catch (SqlException ex)
